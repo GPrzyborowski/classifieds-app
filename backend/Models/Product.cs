3aca4b670e7e5fd92e943aa1552e4c7efe21d2e9
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-25 23:35:13 +0200

    create product model

---
+++ backend/Models/Product.cs
@@ -0,0 +1,11 @@
+public class Product
+{
+    public int Id {get; set;}
+    public string Title {get; set;} = string.Empty;
+
+    public bool Condition {get; set;}
+
+    public decimal Price {get; set;}
+
+    public string Description {get; set;} = string.Empty;
+}

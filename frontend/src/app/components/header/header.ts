a679dfcde51bdedddf1bae1459a0701c29efa8d0
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-25 23:11:23 +0200

    init repo and create project boilerplate

---
+++ frontend/src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  @Input() text?: string
+}

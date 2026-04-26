e711df8ba6d1c27d23232979c35158678d5b68bd
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-26 03:48:49 +0200

    map product endpoints

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 var app = builder.Build();
+
+app.MapProductEndpoints();
 
 app.Run();

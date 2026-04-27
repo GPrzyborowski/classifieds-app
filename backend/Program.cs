217a3d472a83e8478c07b98e704034d08de77ae1
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-27 03:47:02 +0200

    set cors origin

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,9 +6,19 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
-
+app.UseCors("AllowAngular");
 app.MapProductEndpoints();
 
 app.Run();

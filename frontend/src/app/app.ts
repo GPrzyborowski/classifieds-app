9b0c4050e1f69b70db6f50995b814e6706819a6b
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-27 02:43:02 +0200

    add navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
